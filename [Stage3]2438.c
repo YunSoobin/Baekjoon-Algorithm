0153abc3c6b6e83d0b39947e19437d977ab7d0b7
YunSoobin <soobin5296@gmail.com>
2021-01-11 18:27:28 +0900

    Stage3/for문

---
+++ [Stage3]2438.c
@@ -0,0 +1,26 @@
+/*
+<문제>
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+<입력>
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+<출력>
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+*/
+
+#include<stdio.h>
+
+int main(void) { 
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
